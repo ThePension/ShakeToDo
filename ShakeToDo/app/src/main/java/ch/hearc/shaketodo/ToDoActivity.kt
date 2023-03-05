f43e814cada1568592f50d396603f100036a5230
Aubert Nicolas <nicolas.aubert@he-arc.ch>
2023-03-05 10:43:22 +0100

    Adding shake detection (#26)

--- ShakeToDo/app/src/main/java/ch/hearc/shaketodo/ToDoActivity.kt
+++ ShakeToDo/app/src/main/java/ch/hearc/shaketodo/ToDoActivity.kt
@@ -4,6 +4,11 @@ import android.net.Uri
 import android.content.Intent
 import android.app.RecoverableSecurityException
 import android.content.ContentResolver
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -15,7 +20,9 @@ import ch.hearc.shaketodo.database.AppDatabase
 import ch.hearc.shaketodo.model.ToDo
 import ch.hearc.shaketodo.model.ToDoDao
 import java.security.AccessController.getContext
+import java.util.*
 import java.util.concurrent.Executors
+import kotlin.math.sqrt
 
 
 class ToDoActivity : AppCompatActivity() {
@@ -24,9 +31,22 @@ class ToDoActivity : AppCompatActivity() {
     private lateinit var todo : ToDo
     private lateinit var todoDao : ToDoDao
 
+    // Used for shake detection
+    private var sensorManager: SensorManager? = null
+    private var acceleration = 0f
+    private var currentAcceleration = 0f
+    private var lastAcceleration = 0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo)
+
+        // Getting the Sensor Manager instance
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        Objects.requireNonNull(sensorManager)!!
+            .registerListener(sensorListener, sensorManager!!
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
 
         val todoId = intent.getLongExtra("todoId", -1L)
         val database: AppDatabase by lazy { AppDatabase.getInstance(this) }
@@ -54,6 +74,32 @@ class ToDoActivity : AppCompatActivity() {
             todoPriority.rating = rating / 2F
             imageView?.setImageURI(Uri.parse(todo.imagelocation))
         }
+    }
+
+    private val sensorListener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            // Fetching x,y,z values
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+            lastAcceleration = currentAcceleration
+
+            // Getting current accelerations
+            // with the help of fetched x,y,z values
+            currentAcceleration = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
+            val delta: Float = currentAcceleration - lastAcceleration
+            acceleration = acceleration * 0.9f + delta
+
+            // Display a Toast message if
+            // acceleration value is over 12
+            if (acceleration > 10) {
+                Toast.makeText(applicationContext, "Shake event detected", Toast.LENGTH_SHORT)
+                    .show()
+                Log.i("ToDoActivity", "Acceleration changed to : $acceleration")
+            }
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
     }
 
     public fun onDeleteButtonClick(view: View) {
