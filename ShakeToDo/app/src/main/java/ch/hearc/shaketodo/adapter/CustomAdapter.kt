f4b271dc9b915ac818e386a39fd42a1977f4d649
Nicolas Aubert <nicolas.aubert1@he-arc.ch>
2023-05-04 13:43:54 +0200

    Add rating bar + due date in list view design (#14)

--- ShakeToDo/app/src/main/java/ch/hearc/shaketodo/adapter/CustomAdapter.kt
+++ ShakeToDo/app/src/main/java/ch/hearc/shaketodo/adapter/CustomAdapter.kt
@@ -1,14 +1,13 @@
 package ch.hearc.shaketodo.adapter
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.RatingBar
-import android.widget.TextView
+import android.widget.*
 import ch.hearc.shaketodo.R
 import ch.hearc.shaketodo.model.ToDo
 
@@ -20,6 +19,8 @@ class CustomAdapter(private val context: Context, private val data: List<ToDo>) 
         val notes : TextView
         val imageView : ImageView
         val todo_priority : RatingBar
+        val todo_duedate : TextView
+        val parent_layout : LinearLayout
 
         if (convertView == null) {
             view = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false)
@@ -29,6 +30,16 @@ class CustomAdapter(private val context: Context, private val data: List<ToDo>) 
             imageView?.setImageURI(Uri.parse(data[position].imagelocation))
             todo_priority = view.findViewById(R.id.todo_priority)
             todo_priority.rating = (data[position].priority ?: 0) / 2F
+
+            todo_duedate = view.findViewById(R.id.todo_duedate)
+            todo_duedate.text = data[position].duedate
+
+            // If the task is done
+            parent_layout = view.findViewById(R.id.parent_layout)
+            if (data[position].completed == true) {
+                // Strike the text
+                name.paintFlags = name.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+            }
         } else {
             view = convertView
         }
--- ShakeToDo/app/src/main/java/ch/hearc/shaketodo/ui/home/HomeFragment.kt
+++ ShakeToDo/app/src/main/java/ch/hearc/shaketodo/ui/home/HomeFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.LinearLayout
 import android.widget.ListView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
